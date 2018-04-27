341dc3800b2cfd8dd500772fab7d68d98bc9fe57
Igor Baranov <i.baranov@virtusystems.ru>
2018-04-27 19:43:31 +0300

    config and data

---
+++ frontend/src/components/ChampList/ChampList.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Grid, Card, Image, Segment, Dimmer, Loader } from 'semantic-ui-react';
+
+
+
+class UserCard extends Component {
+  constructor(props, context) {
+    super(props, context)
+  }
+
+  render() {
+    const { masteryData, loading } = this.props
+    console.log(this.props)
+    return (
+
+    )
+  }
+}
+
+export default UserCard;
