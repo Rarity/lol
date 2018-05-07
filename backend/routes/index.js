dec25870b92fa0cb1c7a4cf5ed5d73e829774638
Igor Baranov <i.baranov@virtusystems.ru>
2018-05-07 18:40:17 +0300

    apikey to .env

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+import { Router } from 'express';
+
+import dotenv from 'dotenv'
+
+const router = Router()
+dotenv.config()
+
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title:  process.env.API_KEY });
 });
 
 module.exports = router;
